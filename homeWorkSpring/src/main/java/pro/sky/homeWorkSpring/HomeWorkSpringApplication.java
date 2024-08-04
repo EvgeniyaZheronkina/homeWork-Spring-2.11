39b70fda0fef9016427d3c7202939cdda7a67197
zhero <zheronkina86@gmail.com>
2024-08-05 00:16:06 +0300

    New

---
+++ homeWorkSpring/src/main/java/pro/sky/homeWorkSpring/HomeWorkSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeWorkSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkSpringApplication.class, args);
+	}
+
+}
---
+++ homeWorkSpring/src/test/java/pro/sky/homeWorkSpring/HomeWorkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homeWorkSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
